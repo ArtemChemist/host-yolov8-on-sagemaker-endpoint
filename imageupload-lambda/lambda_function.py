3c19d32b16f3722c3c0e55ea5ab6fcfeecbcf479
Artem Lebedev <lebedevfedora@gmail.com>
2024-12-20 06:59:10 -0500

    Updated gitignore and lambda

--- imageupload-lambda/lambda_function.py
+++ imageupload-lambda/lambda_function.py
@@ -18,7 +18,7 @@ def array_to_str(im_array):
     return encoded_im.decode('utf-8')
 
 def lambda_handler(event, context):
-    ENDPOINT_NAME = 'yolov8-pytorch-2024-07-18-21-09-58-005126'            
+    ENDPOINT_NAME = 'YOLOv8-CFU-SageMaker-endpoint'            
     
     # Read the image into a numpy  array
     #orig_image = cv2.imread('test_image.jpg')
